68d68d0465114f2d843d58e1726df9138e241d2f
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2024-05-23 21:33:31 -0400

    events added

--- FaceFacts/EditPersonView.swift
+++ FaceFacts/EditPersonView.swift
@@ -5,10 +5,18 @@
 //  Created by Gabriel Marquez on 2024-05-22.
 //
 
+import SwiftData
 import SwiftUI
 
 struct EditPersonView: View {
+    @Environment(\.modelContext) var modelContext
     @Bindable var person: Person
+    @Binding var navigationPath: NavigationPath
+    
+    @Query(sort: [
+        SortDescriptor(\Event.name),
+        SortDescriptor(\Event.location)
+    ]) var events: [Event]
     
     var body: some View {
         Form {
@@ -21,16 +29,50 @@ struct EditPersonView: View {
                     .textInputAutocapitalization(.never)
             }
             
+            Section("Where did you meet them?") {
+                Picker("Met at", selection: $person.metAt) {
+                    Text("Unknown event")
+                        .tag(Optional<Event>.none)
+                    
+                    if events.isEmpty == false {
+                        Divider()
+                        
+                        ForEach(events) { event in
+                            Text(event.name)
+                                .tag(Optional(event))
+                        }
+                    }
+                }
+                
+                Button("Add a new event", action: addEvent)
+            }
+            
             Section("Notes") {
                 TextField("Details about this person", text: $person.details, axis: .vertical)
             }
         }
         .navigationTitle("Edit Person")
         .navigationBarTitleDisplayMode(.inline)
+        .navigationDestination(for: Event.self) { event in
+            EditEventView(event: event)
+        }
+    }
+    
+    func addEvent() {
+        let event = Event(name: "", location: "")
+        modelContext.insert(event)
+        navigationPath.append(event)
     }
 }
 
-//#Preview {
-//    EditPersonView()
-//}
+#Preview {
+    do {
+        let previewer = try Previewer()
+        
+        return EditPersonView(person: previewer.person, navigationPath: .constant(NavigationPath()))
+            .modelContainer(previewer.container)
+    } catch {
+        return Text("Failed to create preview: \(error.localizedDescription)")
+    }
+}
 
